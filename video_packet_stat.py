06a67607dc5bed22cd78c236995b20cfbf2d57bd
allen <r09922137@ntu.edu.tw>
2021-11-10 23:55:59 +0800

    sync

--- video_packet_stat.py
+++ video_packet_stat.py
@@ -65,12 +65,11 @@ def opendump(ppp1):
                 p2.send("End")
 
 
-pp1, cpp1 = Pipe()
-vlcp = Process(target=openVLC)
-vlcp.start()
-time.sleep(3)
-s = "ffmpeg -i rtsp://127.0.0.1:8554/ -codec copy /home/mvnl/Filmtest/output/abc.mp4"
+#pp1, cpp1 = Pipe()
+#vlcp = Process(target=openVLC)
+#vlcp.start()
+#time.sleep(3)
+#s = "ffmpeg -i rtsp://127.0.0.1:8554/ -codec copy /home/mvnl/Filmtest/output/abc.mp4"
 #subprocess.run(s.split())
 opendump(cpp1)
-
-pp1.send("End")
+#pp1.send("End")
