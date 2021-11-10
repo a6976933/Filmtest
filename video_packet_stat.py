3c3aed065d100f33a08c88abe614ef5308330b65
allen <r09922137@ntu.edu.tw>
2021-11-10 22:43:52 +0800

    sync

---
+++ video_packet_stat.py
@@ -0,0 +1,29 @@
+import os
+import argparse
+import time
+#import numpy as np
+import logging
+import subprocess
+from datetime import datetime
+from multiprocessing import Process, Lock, Pipe
+from os.path import isfile, isdir, join
+import random
+
+def openVLC():
+    s = "vlc test.mp4  --play-and-exit --quiet :sout=#rtp{sdp=rtsp://:8554/} :sout--all :sout-keep"
+    subprocess.run(s.split())
+
+def openpcap():
+    pass
+s = "sudo lsof -i -P -n | grep vlc"
+proc = subprocess.Popen(s, stdout=subprocess.PIPE)
+while True:
+    line = proc.stdout.readline().decode().split()
+    print(line)
+    print(line[9])
+    
+
+#s = "ffmpeg -i rtsp://127.0.0.1:8554-codec copy -r 30 /home/mvnl/output/abc.mp4"
+#subprocess.run(s.split())
+
+
